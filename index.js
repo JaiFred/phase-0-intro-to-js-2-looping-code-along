c8dadcb8f0c227fa2e53032b7321dd337b1cd249
JaiVon <frederickjaivon@yahoo.com>
2022-06-12 16:37:09 -0400

    commit tree b26ecaca7677cf305eaf286d25b52a861e8ee458

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const friends = ['Guadalupe', 'Ollie', 'Aki'];
+
+
+function writeCards(friends) {
+    const cards = [] 
+for (let i = 0 ; i < friends.length; i++){
+    cards.push(`Thank you, ${friends[i]}, for the wonderful surprise gift!`);
+    } 
+    return cards;
+}
+writeCards(friends)
+
+function countDown(){
+    let countDown = 10
+    while (countDown >= 0){
+        console.log(countDown--);
+    }
+}
+
+//console.log(`Thank you, ${friends[i]}, for the wonderful surprise gift!`);
